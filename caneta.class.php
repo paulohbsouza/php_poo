badc3239252b1d9072987bb790670e7ae0716ece
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-02-16 16:01:59 -0300

    Create caneta.class.php

---
+++ caneta.class.php
@@ -0,0 +1,32 @@
+<?php
+
+	/**
+	**/
+
+	class Caneta {
+		var $modelo;
+		var $cor;
+		var $ponta;
+		var $carga;
+		var $tampada;
+ 
+		function rabiscar() {
+			if ($this->tampada == true) {
+				echo "ERRO! Não posso rabiscar!";
+			} else {
+				echo "Estou rabiscando...";
+			}
+			
+		}
+
+		function tampar() {
+			$this->tampada = true;
+		}
+		
+		function destampar() {
+			$this->tampada = false;
+		}
+
+	}
+
+?>
