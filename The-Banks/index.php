4de4d6e754161eb5f7fd4a9db9195b76c55a54a6
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-02-19 10:56:20 -0300

    Update index.php

--- The-Banks/index.php
+++ The-Banks/index.php
@@ -11,11 +11,38 @@
     <title>Olá, mundo!</title>
   </head>
   <body>
-    
-    <h4>Aula 05</h4>
+    <h4>Aula 05 - The Banks</h4>
     <pre>
     <?php
-    	
+    	require_once 'conta_bancaria.class.php';
+
+        $p1 = new ContaBanco();
+        $p2 = new ContaBanco();
+        $p1->abrirConta("CC");
+        $p1->setDono("P1");
+        $p1->setNumConta(111);
+        $p2->abrirConta("CP");
+        $p2->setDono("P2");
+        $p2->setNumConta(222);
+
+        $p1->depositar(300);
+        $p2->depositar(500);
+
+        $p1->sacar(100);
+        $p2->sacar(1000);
+  
+        $p1->pagarMensal();
+        $p2->pagarMensal();
+
+        $p1->sacar(238);
+        //$p2->sacar();
+
+        $p1->fecharConta();
+        $p2->fecharConta();
+
+        print_r($p1);
+        print_r($p2);
+
     ?>
     </pre>
 
