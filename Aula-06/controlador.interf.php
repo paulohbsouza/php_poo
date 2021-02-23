2848a4729f56bdab0bf763a3966f831857014422
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-02-23 18:21:14 -0300

    Create controlador.interf.php

---
+++ Aula-06/controlador.interf.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	# Interface implementada por uma classe.
+	**/
+	Interface Controlador {
+
+		public abstract function ligar();
+		public abstract function desligar();
+		public abstract function abrirMenu();
+		public abstract function fecharMenu();
+		public abstract function maisVolume();
+		public abstract function menosVolume();
+		public abstract function ligarMudo();
+		public abstract function play();
+		public abstract function pause();
+		
+	}
+
+?>
+
+
