a724a713fd0561692b8c45f24bd1016cc0c3a5c4
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-03-04 17:33:01 -0300

    Update index.php

--- Aula-07/index.php
+++ Aula-07/index.php
@@ -15,9 +15,17 @@
     <pre>
     <?php
     	require_once 'lutador.class.php';
-    	$l = new Lutador("Pretty Boy", "França", 30, 1.75, 68.9, 11, 2, 1);
+    	$l = array();
+    	$l[0] = new Lutador("Pretty Boy", "França", 30, 1.75, 90.9, 11, 2, 1);
+    	$l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+    	$l[2] = new Lutador("SnapShadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+    	$l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+    	$l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+    	$l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
     	//print_r($l);
-    	$l->status();
+    	$l[2]->perderLuta();
+    	$l[2]->apresentar();
+    	$l[2]->status();
     ?>
     </pre>
 
