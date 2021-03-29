cb43b465d7154bddac9c73824932966b373679fe
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-03-29 17:59:00 -0300

    Update index.php

--- Aula-14/index.php
+++ Aula-14/index.php
@@ -16,12 +16,17 @@
     <?php
 
         require_once 'video.class.php';
+        require_once 'gafanhoto.class.php';
 
         $v[0] = new Video ("Aula 1 de POO");
         $v[1] = new Video ("Aula 12 de PHP");
         $v[2] = new Video ("Aula 15 de HTML5");
 
+        $g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
+        $g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
+
         print_r($v);
+        print_r($g);
 
     ?>
     </pre>
